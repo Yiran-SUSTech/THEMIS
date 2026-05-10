24a0f66e1933659792df4612e46650f5e8b0c14c
Yiran <yiranzhang3502@outlook.com>
2026-05-10 15:42:21 +0800

    test api

--- test_api2.py
+++ test_api2.py
@@ -7,7 +7,7 @@ client = OpenAI(
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
-messages = [{"role": "user", "content": "你是谁"}]
+messages = [{"role": "user", "content": "who are you?"}]
 completion = client.chat.completions.create(
     model="qwen3.6-plus",  # 您可以按需更换为其它深度思考模型
     messages=messages,
@@ -15,7 +15,7 @@ completion = client.chat.completions.create(
     stream=True
 )
 is_answering = False  # 是否进入回复阶段
-print("\n" + "=" * 20 + "思考过程" + "=" * 20)
+print("\n" + "=" * 20 + "Thinking Process" + "=" * 20)
 for chunk in completion:
     delta = chunk.choices[0].delta
     if hasattr(delta, "reasoning_content") and delta.reasoning_content is not None:
@@ -23,6 +23,6 @@ for chunk in completion:
             print(delta.reasoning_content, end="", flush=True)
     if hasattr(delta, "content") and delta.content:
         if not is_answering:
-            print("\n" + "=" * 20 + "完整回复" + "=" * 20)
+            print("\n" + "=" * 20 + "Full Reply" + "=" * 20)
             is_answering = True
         print(delta.content, end="", flush=True)
