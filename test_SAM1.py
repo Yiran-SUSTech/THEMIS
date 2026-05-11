0c6b683b1d54dc291f2401fda52dbd9cee1f8c00
Yiran <yiranzhang3502@outlook.com>
2026-05-12 01:01:34 +0800

    test api

--- test_SAM1.py
+++ test_SAM1.py
@@ -6,8 +6,8 @@ import os
 # --- 1. 配置路径 ---
 MODEL_DIR = "new_models/sam1_onnx/machine_learning_models/"
 # 建议先用 MobileSAM 测试，如果追求精度再换 sam_vit_h
-ENCODER_PATH = os.path.join(MODEL_DIR, "mobile_sam.encoder.onnx")
-DECODER_PATH = os.path.join(MODEL_DIR, "mobile_sam.decoder.onnx")
+ENCODER_PATH = os.path.join(MODEL_DIR, "mobile_sam.encoder_v16.onnx")
+DECODER_PATH = os.path.join(MODEL_DIR, "mobile_sam.decoder_v16.onnx")
 IMAGE_PATH = "./test_images/hussar monkey.png" 
 
 # --- 2. 初始化 ONNX Session ---
